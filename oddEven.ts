c795233195547e56730e9e1227fdbc0c2f620927
trevor nkuna <trevornkuna7@gmail.com>
2023-04-14 10:24:33 +0200

    second commti

---
+++ oddEven.ts
@@ -0,0 +1,17 @@
+function oddEven(firstNumber: number):number{
+
+
+    if(firstNumber%2==0){
+
+        console.log("Even number")
+    }
+
+    else{
+        
+        console.log("Odd number")
+
+    }
+    return firstNumber;
+   
+}
+console.log(oddEven(7));
